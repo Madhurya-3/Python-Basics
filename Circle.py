5fcab5c81c19f65a4bdbd6c1ec38aabf53aa4028
Madhurya <85580048+Madhurya-3@users.noreply.github.com>
2023-08-12 20:07:33 +0530

    Created Area of Circle

---
+++ Circle.py
@@ -0,0 +1,7 @@
+r = float(input("Input the radius of a circle: "))
+area = (22/7)*r*r
+print("The Area of the circle with radius ",r,"is ",area)
+
+
+# Input the radius of a circle: 4
+# The Area of the circle with radius  4.0 is  50.285714285714285
