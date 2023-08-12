15e809b5dffb3122e6f2e2a7ff7d7293b845a43e
Madhurya <85580048+Madhurya-3@users.noreply.github.com>
2023-08-12 20:09:53 +0530

    Created FileExtension.py
    Returns the extension of any file

---
+++ FileExtension.py
@@ -0,0 +1,6 @@
+filename = input("Input any Filename: ")
+f = filename. split(".")
+print ("The extension of the file is : " + repr(f[-1]))
+
+# Input any Filename: www.gmail.com
+# The extension of the file is : 'com'
